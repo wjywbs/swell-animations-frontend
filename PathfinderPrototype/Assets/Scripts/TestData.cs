d4df4ffbed24014d185db49e53ced3faf855dc42
Angel Delgado <angeldelgado7@gmail.com>
2016-03-03 19:58:02 -0500

    changed testdata positions based on lines

--- PathfinderPrototype/Assets/Scripts/TestData.cs
+++ PathfinderPrototype/Assets/Scripts/TestData.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using swellanimations;
 
 public class TestData {
-    public static Node[] createTestAnimation(Node firstFrame)
+    public static Node[] createTestAnimation(Node firstFrame, List<Vector3> points)
     {
-        Node[] frames = new Node[100];
+        Node[] frames = new Node[points.Count];
         for(int x = 0; x < frames.Length; x++)
         {
+            firstFrame.positionX = points[x].x;
+            firstFrame.positionY = points[x].y;
+            firstFrame.positionZ = points[x].z;
             frames[x] = createFrame(firstFrame, x);
         }
         return frames;
