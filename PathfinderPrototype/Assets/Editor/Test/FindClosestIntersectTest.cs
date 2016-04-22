e0c51382cbfe86895667f953982df3d8e8065f94
Angel Delgado <angeldelgado7@gmail.com>
2016-04-21 21:26:54 -0400

    Closest intersect helper class passes all unit tests.

--- PathfinderPrototype/Assets/Editor/Test/FindClosestIntersectTest.cs
+++ PathfinderPrototype/Assets/Editor/Test/FindClosestIntersectTest.cs
@@ -5,30 +5,52 @@ using System.Collections.Generic;
 
 [TestFixture]
 [Category("Helper Class Tests")]
-public class FindClosestIntersectTest : MonoBehaviour {
+public class FindClosestIntersectTest : MonoBehaviour
+{
 
-
-    [Test]
-    public void findClosestPointTest()
+    private List<Vector3> createCurve()
     {
-        float tolerance = 1.0f;
-        FindClosestIntersect intersectFinder = new FindClosestIntersect();
-        intersectFinder.searchTolerance = tolerance;
-
-        IList<Vector3> points = new List<Vector3>();
+        List<Vector3> points = new List<Vector3>();
         points.Add(new Vector3(1, 1, 0));
         points.Add(new Vector3(2, 2, 0));
         points.Add(new Vector3(3, 1, 0));
         points.Add(new Vector3(5, 3, 0));
         points.Add(new Vector3(6, 2, 0));
 
+        return points;
+    }
+
+    [Test]
+    public void testIntersectPoint()
+    {
         Vector3 testPoint = new Vector3(4, 2, 0);
 
-        Vector3? closestPoint = intersectFinder.search(points, testPoint);
+        Vector3 closestPoint = FindClosestIntersect.search(createCurve(), testPoint);
 
         Assert.That(closestPoint, Is.Not.Null);
-        Assert.That(Vector3.Distance(testPoint, closestPoint.Value), Is.LessThanOrEqualTo(tolerance));
+        Assert.That(closestPoint, Is.EqualTo(testPoint));
+    }
 
+    [Test]
+    public void testClosePoint()
+    {
+        Vector3 testPoint = new Vector3(4, 2.1f, 0);
+
+        Vector3 closestPoint = FindClosestIntersect.search(createCurve(), testPoint);
+
+        Assert.That(closestPoint, Is.Not.Null);
+        Assert.That(Vector3.Distance(closestPoint, testPoint), Is.LessThanOrEqualTo(0.1f));
+    }
+
+    [Test]
+    public void testFarPoint()
+    {
+        Vector3 testPoint = new Vector3(5, 13, 0);
+
+        Vector3 closestPoint = FindClosestIntersect.search(createCurve(), testPoint);
+
+        Assert.That(closestPoint, Is.Not.Null);
+        Assert.That(Vector3.Distance(closestPoint, testPoint), Is.GreaterThanOrEqualTo(10));
     }
 
 }
--- PathfinderPrototype/Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ PathfinderPrototype/Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 namespace UnityTest
 {
+    [Ignore]
     [TestFixture]
     [Category("Sample Tests")]
     internal class SampleTests
