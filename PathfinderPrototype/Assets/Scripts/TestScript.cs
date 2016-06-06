e3370628c85bc8f98c4e8568164ecb00ec4e135a
Angel Delgado <angeldelgado7@gmail.com>
2016-06-06 09:26:56 -0400

    Added demo stuff

--- PathfinderPrototype/Assets/Scripts/TestScript.cs
+++ PathfinderPrototype/Assets/Scripts/TestScript.cs
@@ -4,14 +4,20 @@ using System.Collections;
 [AddComponentMenu("Animation/Animation Tester")]
 public class TestScript : MonoBehaviour {
 
+    public float delay = 0f;
+    private bool started = false;
+
 	// Use this for initialization
 	void Start () {
-        AnimationPlayer animationPlayer = gameObject.GetComponent<AnimationPlayer>();
-        animationPlayer.ToggleAnimation();
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Time.fixedTime > delay && !started)
+        {
+            started = true;
+            AnimationPlayer animationPlayer = gameObject.GetComponent<AnimationPlayer>();
+            animationPlayer.ToggleAnimation();
+        }
+    }
 }
